46c50d8493fe6e9154903c9e3304fa8fcb9421bf
AllegriM <elmonky99@gmail.com>
2022-11-30 20:49:30 -0300

    fix: :bug: Resolve merge conflict in Day 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 
 function App() {
+  const regalos : string[] = ["Medias", "Zapatillas", "Teclado"]
 
   return (
-    <div className="App"></div>
+    <div className="App">
+      <h2>Regalos</h2>
+      <ul>
+        {
+          regalos.map(regalo => {
+            return(
+              <li key={regalo}>
+                {regalo}
+              </li>
+            )
+          })
+        }
+      </ul>
+    </div>
   )
 }
 
