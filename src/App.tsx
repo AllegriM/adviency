ef33e3ec120f8cde5cebda6e8fc6003cb839af42
AllegriM <elmonky99@gmail.com>
2022-11-30 21:18:30 -0300

    feat: :sparkles: Adding some christmas colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
+import { useState } from "react"
 
 function App() {
 
+  const [gifts, setGifts] = useState<string[]>(["Medias", "Botines", "Copa"])
+
   return (
-    <div className="App"></div>
+    <div className="App min-h-screen flex items-center bg-gradient-to-b from-rose-500 via-rose-800 to-green-900 ">
+      <div className="bg-white text-neutral-800 min-w-[300px] h-72 m-auto p-6 rounded-md border-dotted border-amber-400 border-8">
+        <h2 className="text-4xl font-bold mb-3">Regalos:</h2>
+        <ul>
+          {
+            gifts.map(gift => {
+              return(
+                <li key={gift}>
+                  {gift}
+                </li>
+              )
+            })
+          }
+        </ul>
+      </div>
+    </div>
   )
 }
 
