8169c55d5e947928c801b9dabba78386d0170b73
AllegriM <elmonky99@gmail.com>
2022-12-29 17:45:12 -0300

    feat: :sparkles: Day10: Adding input to add images

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,16 @@ import React, { useEffect, useState } from "react"
 interface Gift {
   name: string
   amount: number
+  image: string
 }
 
 function App() {
 
-  const defaultGifts : Gift[] = [{name: "Fernet", amount: 2}, {name: "Coca", amount: 4}, {name: "Hielo" , amount: 1}]
+  const defaultGifts : Gift[] = [{name: "Fernet", amount: 2, image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Fernetbranca.jpg/399px-Fernetbranca.jpg"}, {name: "Coca", amount: 4, image: "https://thumbs.dreamstime.com/b/frasco-de-coca-cola-con-gotas-agua-aisladas-sobre-fondo-blanco-mosc%C3%BA-rusia-noviembre-165678471.jpg"}, {name: "Hielo" , amount: 1, image: "https://media.istockphoto.com/id/177131518/es/foto/cubos-de-hielo.jpg?s=612x612&w=0&k=20&c=71wDtnaGnZJVXV9NFsU2wl9H6AGbzICa2uiufEUv8nY="}]
 
   const [gifts, setGifts] = useState<Gift[]>(() => JSON.parse(localStorage.getItem("gifts")!) || defaultGifts)
 
-  const [newGift, setnewGift] = useState<Gift>({name: "", amount: 1})
+  const [newGift, setNewGift] = useState<Gift>({name: "", amount: 1, image: ""})
 
   const addGift = (e: React.FormEvent) => {
     e.preventDefault()
@@ -20,11 +21,16 @@ function App() {
       return
     }
     setGifts([...gifts, newGift])
+    setNewGift({
+      name: "",
+      amount: 1,
+      image: ""
+    })
   }
   
   const handleGiftData = (e: React.FormEvent<HTMLInputElement>) => {
     const {name, value} = e.currentTarget
-    setnewGift({
+    setNewGift({
       ...newGift,
       [name]: value
     })
@@ -46,12 +52,19 @@ function App() {
     <div className="App min-h-screen font-bold grid place-items-center bg-red-900">
       <div className="bg-green-900 p-6 rounded-md">
         <form onSubmit={addGift}>
-          <div className="">
-            <label className="block" htmlFor="giftName">Nombre del regalo</label>
-            <input onChange={handleGiftData} type="text" name="name" id="giftName" className="px-2" />
-            <input onChange={handleGiftData} type="number" name="amount" min={1} className="w-14 ml-4 pl-3" />
+          <div className="flex items-baseline mb-2">
+            <div>
+              <label className="block" htmlFor="giftName">Nombre del regalo</label>
+              <input onChange={handleGiftData} value={newGift.name} type="text" name="name" id="giftName" className="px-2 rounded-md" />
+            </div>
+            <div className="ml-4">
+              <label className="block" htmlFor="amount">Amount</label>
+              <input onChange={handleGiftData} value={newGift.amount} type="number" id="amount" name="amount" min={1} className="w-28 pl-3 rounded-md" />            
+            </div>            
           </div>
-          <button className="w-full bg-red-900 mt-3 py-1 rounded-md">Agregar regalo🎁</button>
+          <label htmlFor="image">Imagen</label>
+          <input onChange={handleGiftData} value={newGift.image} type="text" id="image" name="image" className="mb-2 w-full px-2 rounded-md" />
+          <button className="w-full bg-red-900 py-1 rounded-md">Agregar regalo🎁</button>
         </form>
         {
           gifts.length ?
@@ -64,7 +77,7 @@ function App() {
             gifts.map(gift => {
               return(
                 <li className="flex gap-4 px-4 my-4 items-center" key={gift.name}>
-                  <span className="text-xl">🎄</span>
+                  <img src={gift.image} alt={gift.name} className="object-contain w-[100px] h-[100px]" />
                   <p className="flex-grow">{gift.name} ({gift.amount})</p>
                   <button onClick={() => deleteGift(gift.name)} className="text-xl">🗑</button>
                 </li>
